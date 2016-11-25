df39a7c81c1fb8c3e2be300dcf70e893c0d4db81
Robert Carll <Robert Carll>
2016-11-25 09:19:12 -0400

    Finished adding lives and setting position to the top right corner

---
+++ RC-Frogger/Frog.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Entity.h"
+#include "Command.h"
+#include "CommandQueue.h"
+#include "ResourceIdentifiers.h"
+
+namespace GEX {
+	class Frog : public Entity
+	{
+	public:
+		enum class Type { Frogger };
+
+	public:
+		Frog(Type type = Type::Frogger);
+		
+		virtual ~Frog() {};
+
+		unsigned int getCategory() const override;
+		sf::FloatRect getBoundingRect() const override;
+
+		void playLocalSound(CommandQueue& commands, SoundEffectID effect);
+		
+
+	private:
+		void updateCurrent(sf::Time dt, CommandQueue& command) override;
+		void movementUpdate(sf::Time dt);
+		void drawCurrent(sf::RenderTarget & target, sf::RenderStates state) const;
+
+	private:
+		Type _type;
+		sf::Sprite _sprite;
+		int _lives;
+		mutable sf::Sprite _life;
+
+
+
+	};
+}
